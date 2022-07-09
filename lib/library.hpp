9f18001238b195602b758e75f3adbccb336e8b8b
nfrmtk <Lykov.ivan.02@mail.ru>
2022-07-09 17:38:47 +0300

    написал тестики, поправил входной/выходной указатель

--- lib/library.hpp
+++ lib/library.hpp
@@ -10,23 +10,29 @@ namespace my{
             short error_count = 0;
             for (int i = 0; i < 256; ++i){
                 if (i == 0){
-                    desired_arr[i] = '\0';
+                    desired_arr[0] = '\0';
                 }
                 else{
-                    std::wstring temp;
-                    std::string temp_desired_locale = std::string("")+=(char)i;
-                    MultiByteToWideChar(
+                    wchar_t letter_answer = L'a';
+                    std::wstring str_answer = L"a";
+                    // std::string temp_desired_locale = std::string("")+=(char)i;
+                    
+                    char letter_to_convert = i;
+                    std::string string_before_convert = "a";
+                    string_before_convert[0] = (char)i;
+
+                    char *str_to_convert = string_before_convert.data();
+                    wchar_t* answer = str_answer.data();
+                    auto err = MultiByteToWideChar(
                             CodePage,
                             NULL,
-                            temp_desired_locale.c_str(),
-                            10,
-                            temp.data(),
+                            str_to_convert,
+                            1,
+                            answer,
                             1
                     );
-                    desired_arr[i] = temp[0];
-                    if (temp[1] != '\0') {
-                        errors[error_count++] = i;
-                    }
+                    desired_arr[i] = *answer;
+                    if (err == 0) errors[i] = 1;
                 }
             }
         }
--- tests/doctest.cpp
+++ tests/doctest.cpp
@@ -10,6 +10,20 @@ TEST_CASE("ru"){
     std::vector<wchar_t> vec(256, 'a');
     std::vector<int> vec_errs(256, 0);
     my::filler::fill(vec.data(), 1251, vec_errs.data());
-    CHECK_EQ(vec[0xC4], L'Д');
-    CHECK_EQ(vec[0xBB], L'»');
+    CHECK_EQ(vec[0xC4], L'\u0414'); // Д
+    CHECK_EQ(vec[0xB8], L'\u0451'); // ё
+    CHECK_EQ(vec[0xBB], L'\xbb'); // »
 }
+
+
+TEST_CASE("de/sp/fr"){
+    std::vector<wchar_t> vec(256, 'a');
+    std::vector<int> vec_errs(256, 0);
+    my::filler::fill(vec.data(), 1252, vec_errs.data());
+    CHECK_EQ(vec[0xC0], L'\xC0'); // À
+    CHECK_EQ(vec[0xD8], L'\u00D8'); // Ø
+    CHECK_EQ(vec[0xBC], L'\xbc'); // ¼
+    CHECK_EQ(vec[0xBF], L'\xbf'); // ¿
+}
+
+
