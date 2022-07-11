352d8a2d8c43be1e58ad676439109a7973fdeebb
nfrmtk <Lykov.ivan.02@mail.ru>
2022-07-11 22:16:16 +0300

    написал вспомогательные функции и большой тестик, который переводит слово.

--- lib/library.hpp
+++ lib/library.hpp
@@ -4,39 +4,35 @@
 #include <iostream>
 
 namespace my{
-    class filler{
-    public:
-        static void fill (wchar_t * desired_arr, INT CodePage, int* errors){
-            short error_count = 0;
-            for (int i = 0; i < 256; ++i){
-                if (i == 0){
-                    desired_arr[0] = '\0';
-                }
-                else{
-                    wchar_t letter_answer = L'a';
-                    std::wstring str_answer = L"a";
-                    // std::string temp_desired_locale = std::string("")+=(char)i;
-                    
-                    char letter_to_convert = i;
-                    std::string string_before_convert = "a";
-                    string_before_convert[0] = (char)i;
-
-                    char *str_to_convert = string_before_convert.data();
-                    wchar_t* answer = str_answer.data();
-                    auto err = MultiByteToWideChar(
-                            CodePage,
-                            NULL,
-                            str_to_convert,
-                            1,
-                            answer,
-                            1
-                    );
-                    desired_arr[i] = *answer;
-                    if (err == 0) errors[i] = 1;
-                }
-            }
+    void fill_alphabet (char16_t * desired_arr/*size=256*/, INT CodePage, int* errors/*size=256*/){
+        desired_arr[0] = '\0';
+        for (uint8_t i = 1; i != 0; ++i){
+            wchar_t answer[4];
+            char str_to_convert[2];
+            str_to_convert[0] = i;
+            str_to_convert[1] = '\0';
+            auto err = MultiByteToWideChar(
+                    CodePage,
+                    NULL,
+                    str_to_convert,
+                    1,
+                    answer,
+                    1
+            );
+            desired_arr[i] = answer[0];
+            if (err == 0) errors[i] = 1;
+            if (answer[1] != 52428)
+                errors[i] = 1;
         }
-    };
+    }
+    void translate_word_given_alphabet(const char16_t* alphabet, const char* word, char16_t* answer, INT CodePage) {
+        
+        
+        for (size_t i = 0; word[i] != '\0'; ++i) {
+            word[i] >= 0 ? answer[i] = alphabet[word[i]] :
+                answer[i] = alphabet[256 + word[i]];
+        }
+    }
 } // my
 
 
--- tests/doctest.cpp
+++ tests/doctest.cpp
@@ -4,26 +4,68 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 
 #include <library.hpp>
-#include "doctest/doctest.hpp"
+#include <doctest/doctest.hpp>
+
+void check_errs_vector(const std::vector<int>& vec_errs){
+    uint8_t errs_total = 0;
+    std::for_each(vec_errs.begin(), vec_errs.end(), [&errs_total](const int& err) -> void {
+        ++errs_total;
+    });
+    CHECK_EQ(errs_total, 0);
+}
+
+
 
 TEST_CASE("ru"){
-    std::vector<wchar_t> vec(256, 'a');
+    std::vector<char16_t> vec(256, 'a');
     std::vector<int> vec_errs(256, 0);
-    my::filler::fill(vec.data(), 1251, vec_errs.data());
+    my::fill_alphabet(vec.data(), 1251, vec_errs.data());
     CHECK_EQ(vec[0xC4], L'\u0414'); // Д
     CHECK_EQ(vec[0xB8], L'\u0451'); // ё
-    CHECK_EQ(vec[0xBB], L'\xbb'); // »
+    CHECK_EQ(vec[0xBB], L'\u00bb'); // »
+    check_errs_vector(vec_errs);
 }
 
 
 TEST_CASE("de/sp/fr"){
-    std::vector<wchar_t> vec(256, 'a');
+    std::vector<char16_t> vec(256, 'a');
     std::vector<int> vec_errs(256, 0);
-    my::filler::fill(vec.data(), 1252, vec_errs.data());
-    CHECK_EQ(vec[0xC0], L'\xC0'); // À
-    CHECK_EQ(vec[0xD8], L'\u00D8'); // Ø
-    CHECK_EQ(vec[0xBC], L'\xbc'); // ¼
-    CHECK_EQ(vec[0xBF], L'\xbf'); // ¿
+    my::fill_alphabet(vec.data(), 1252, vec_errs.data());
+    CHECK_EQ(vec[0xC0], u'\u00C0'); // À
+    CHECK_EQ(vec[0xD8], u'\u00D8'); // Ø
+    CHECK_EQ(vec[0xBC], u'\u00bc'); // ¼
+    CHECK_EQ(vec[0xBF], u'\u00bf'); // ¿
+    check_errs_vector(vec_errs);
 }
 
+
+TEST_CASE("russian locale -> char16_t"){
+    constexpr int ru_code_page = 1251;
+    const size_t mama_size = 5;
+    char MAMA[mama_size];
+    MAMA[0] = 204;
+    MAMA[1] = 192;
+    MAMA[2] = 204;
+    MAMA[3] = 192;
+    MAMA[4] = '\0';
+    char16_t alphabet_to_fill[256];
+    int errs[256];
+
+    my::fill_alphabet(alphabet_to_fill, ru_code_page, errs);
+    char16_t MAMA_wide[mama_size];
+    my::translate_word_given_alphabet(alphabet_to_fill, MAMA, MAMA_wide/*only mutable link*/, ru_code_page);
+
+
+    constexpr char16_t MAMA_by_hand[5] = u"\u041C\u0410\u041C\u0410";
+
+    bool are_equal = true;
+    for (size_t i = 0; i < mama_size - 1; ++i) {
+        if (MAMA_wide[i] != MAMA_by_hand[i]) {
+            are_equal = false;
+            break;
+        }
+    }
+    CHECK(are_equal);
+    // CHECK_EQ(MAMA_wide, MAMA_by_hand);  doesn't work =(
+}
 
